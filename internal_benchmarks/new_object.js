3e65eda9b5e7f1b7f5c5bf089ff8386f67309729
Eugene Korzun <e@korzun.ru>
2020-06-24 19:06:41 +0300

    new object creation benchamrks

---
+++ internal_benchmarks/new_object.js
@@ -0,0 +1,25 @@
+const { Suite } = require('benchmark')
+const suite = new Suite
+
+suite
+  .add('new Object', function () {
+    return new Object
+  })
+  .add('Plain {}', function () {
+    return {}
+  })
+  .add('new Function', function () {
+    return new Function
+  })
+  .add('Object.create', function () {
+    return Object.create(null)
+  })
+
+  .on('cycle', function (event) {
+    console.log(String(event.target));
+  })
+  
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  .run({ 'async': true });
