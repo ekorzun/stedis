231c21fc74ab7145f4d1f9a3a3d141bde49cd250
Eugene Korzun <e@korzun.ru>
2020-06-29 20:21:18 +0300

    context api + exports for gcc

--- src/stedis.js
+++ src/stedis.js
@@ -1,9 +1,21 @@
+// @define process = {}
 import emitterAPI from './emitter'
 
 
 const isDev = process.env.NODE_ENV !== 'production'
 
 const PATH_SEPARATOR = '/'
+const TAG_SEPARATOR = '#'
+const EVENT_TYPE_SEPARATOR = ':'
+
+const EVENT_TYPE_UPDATE = 'update'
+
+const EVENT_TYPE_OBJECT_ADDED = 'added'
+const EVENT_TYPE_OBJECT_REMOVED = 'removed'
+
+const EVENT_TYPE_COLLECTION_ADD = 'add'
+const EVENT_TYPE_COLLECTION_REMOVE = 'remove'
+const EVENT_TYPE_COLLECTION_CHANGE = 'change'
 
 const
 
@@ -197,7 +209,7 @@ const isPrimitiveMap = new Map([
 const uniqID = new function () {
   let id = isDev ? 0 : +new Date
   // Map.get( string ) works faster
-  return () => `_$${++id}`
+  return () => `id_${++id}`
 }
 
 // Map contains source uri path as key
@@ -219,44 +231,82 @@ function createParentEventNames(path) {
   return arr
 }
 
-export function on(path, type = 'change', handler) {
+exports[`on`] = on
+function on(path, type, handler) {
+  if(!type) {
+    type = EVENT_TYPE_UPDATE
+  }
+  if (typeof type === 'object') {
+    for (const k in type) {
+      on(path, k, type[k], handler)
+    }
+    return
+  }
   if (typeof type === 'function') {
     handler = type
-    type = 'change'
+    type = EVENT_TYPE_UPDATE
   }
-  const ekey = `${path}:${type}`
+  path = path.split('#')[0]
+  const ekey = makeEventKey(path, type)
   emitterAPI.on(ekey, handler)
   return () => {
     emitterAPI.off(ekey, handler)
   }
 }
 
-export function once(path, type, handler) {
+exports[`once`] = once
+function once(path, type, handler) {
   if (typeof type === 'function') {
     handler = type
-    type = 'change'
+    type = EVENT_TYPE_UPDATE
   }
-  const ekey = `${path}:${type}`
+  const ekey = makeEventKey(path, type)
   emitterAPI.once(ekey, handler)
 }
 
-export function off(path, type = 'change', handler ) {
-  const ekey = `${path}:${type}`
+exports[`off`] = off
+function off(path, type = EVENT_TYPE_UPDATE, handler ) {
+  const ekey = makeEventKey(path, type)
   emitterAPI.off(ekey, handler)
 }
 
-export function emit(evt, payload) {
-  emitterAPI.emit(evt, payload)
+exports[`emit`] = emit
+function emit(path, type, payload) {
+  emitterAPI.emit(makeEventKey(path, type), payload)
 }
 
-export function notify(path, type = 'change', payload = undefined) {
+function EventObject(path, type, payload, initiator) {
+  this.path = path
+  this.type = type
+  this.initiator = initiator
+  this.payload = payload
+}
+
+function makeEventKey(path, type) {
+  return `${path}${EVENT_TYPE_SEPARATOR}${type}`
+}
+
+
+exports[`notify`] = notify
+function notify(
+  path, 
+  type = EVENT_TYPE_UPDATE, 
+  payload = undefined
+) {
   const allPaths = getParentEventNames(path)
-  for (let i = -1, t = allPaths.length; ++i < t;){
-    emit(`${allPaths[i]}:${type}`, payload)
+  for (let i = -1, t = allPaths.length, p; ++i < t; p = allPaths[i]){
+    emit(p, type, new EventObject(
+        p,
+        type,
+        payload,
+        path,
+      )
+    )
   }
 }
 
-export function computed(path, from, toValue) {
+exports[`computed`] = computed
+function computed(path, from, toValue) {
   const unsubscribe = []
   const setValue = () => {
     set(path, toValue(from.map(f => get(f))))
@@ -331,7 +381,10 @@ const pathsToRemove = []
 function setOrUpdateCollection(path, selector) {
   const objId = objectToIdsMap.get(selector)
   path.pop()
-  const internalCollectionId = getOrCreateIdByPath(path.join('/'))
+
+
+
+  const internalCollectionId = getOrCreateIdByPath(path.join(PATH_SEPARATOR))
   let collection = collections.get(internalCollectionId)
 
   if (!collection) {
@@ -340,10 +393,16 @@ function setOrUpdateCollection(path, selector) {
     collections.set(internalCollectionId, collection)
   }
 
+  const selectorExists = collection.has(selector)
+  if (selectorExists) {
+    return selectorExists
+  }
+
   collection.add(objId)
   // collections.set(objId, true)
   // return collection.keys()
-  return collection
+
+  return false
 }
 
 
@@ -390,13 +449,22 @@ function getSelector(path) {
 }
 
 
-
+// 
+// 
+// 
+exports[`add`] = add
+function add(path, value, nextId) {
+  nextId = nextId || uniqID()
+  set(`${path}/${nextId}`, value)
+  return nextId
+}
 
 
 // 
 // 
 // 
-export function set(_path, value) {
+exports[`set`] = set
+function set(_path, value) {
   const path = parsePath(_path)
 
   if (path.length % 2 === 0) {
@@ -410,7 +478,9 @@ export function set(_path, value) {
 
   objectNeverWasUsed.delete(selector)
 
-  if (isPrimitiveMap.get(typeof value) || value === null) {
+  const isPrimitive = isPrimitiveMap.get(typeof value) || value === null
+
+  if (isPrimitive) {
     
     objectIsPrimitive.set(id, true)
     objectPrimitiveValues.set(selector, value)
@@ -421,17 +491,38 @@ export function set(_path, value) {
     assign(selector, value)
   }
 
-  setOrUpdateCollection(_path.split('/'), selector)
-  notify(_path, 'change', value)
+  // Already exists
+  if (setOrUpdateCollection(_path.split(PATH_SEPARATOR), selector)) {
+
+  } else {
+    // added
+    emit(_path, 'added', isPrimitive ? value : selector)
+    emit(_path.replace(/\/w+$/, '/*'), 'added', isPrimitive ? value : selector)
+    emit(_path.replace(/\/w+$/, ''), 'add', isPrimitive ? value : selector)
+  }
+  
+  if(isPrimitive) {
+    notify(_path, 'update', value)
+  } else {
+    notify(_path, 'update', selector)
+  }
   return value
 }
 
+// 
+// 
+// 
+exports[`next`] = next
+function next( path ) {
+  
+}
 
 // 
 // 
 // 
-export function merge(_path, value) {
-  const path = _path.split('/')
+exports[`merge`] = merge
+function merge(_path, value) {
+  const path = _path.split(PATH_SEPARATOR)
 
   if (path.length % 2 === 0) {
     throw new Error(`merge() method allowed only on objects, not collections. Check your path: ${_path}`)
@@ -446,14 +537,15 @@ export function merge(_path, value) {
 
   assign(selector, value)
 
-  notify(_path, 'change', value)
+  notify(_path, 'update', selector)
   return value
 }
 
 // 
 // 
 // 
-export function meta(path, value = undefined) {
+exports[`meta`] = meta
+function meta(path, value = undefined) {
   const id = getOrCreateIdByPath(path)
   const selector = getOrCreateSelectorById(id)
   if( value === undefined ) {
@@ -467,7 +559,17 @@ export function meta(path, value = undefined) {
 // 
 // 
 const taggedCollectionsMap = new Map
-export function tag(path, tagName, comparator) {
+
+exports[`tag`] = tag
+function tag(path, tagName, comparator) {
+
+  if (typeof tagName === 'object') {
+    for(const k in tagName) {
+      tag(path, k, tagName[k])
+    }
+    return
+  }
+
   const key = `${path}#${tagName}`
   let tg = taggedCollectionsMap.get(key)
   if(!tg) {
@@ -475,7 +577,7 @@ export function tag(path, tagName, comparator) {
     taggedCollectionsMap.set(key, tg)
   }
 
-  on(path, 'change', value => {
+  on(path, 'update', ({payload: value}) => {
     if (comparator(value) === true){
       tg.add(value)
     } else {
@@ -490,8 +592,42 @@ export function tag(path, tagName, comparator) {
   // [...tagA].filter(x => tagB.has(x));
 
 }
+exports[`untag`] = untag
+function untag(path, tagName) {
+  const key = `${path}#${tagName}`
+  return taggedCollectionsMap.delete(key)
+}
 
-export function getTag(path, tag){
+
+function getTag(path, tag){
+  // console.log(path, tag)
+  if(tag.indexOf(',') > -1) {
+    return [...new Set(
+      tag
+        .split(',')
+        .map(t => taggedCollectionsMap.get(`${path}#${t}`))
+        .flat()
+    )]
+  }
+  if(tag.indexOf('&') > -1) {
+    const tags = tag.split('&')
+    const t = tags.pop()
+    const filter = (item) => {
+      for(let i = tags.length; --i > -1;) {
+        const key = `${path}#${tags[i]}`
+        // try {
+          if (!taggedCollectionsMap.get(key).has(item)) {
+            return false
+          }
+        // } catch(e) {
+        //   return false
+        // }
+      }
+      return true
+    }
+    
+    return [...taggedCollectionsMap.get(`${path}#${t}`)].filter(filter)
+  }
   return [...taggedCollectionsMap.get(`${path}#${tag}`)]
 }
 
@@ -525,7 +661,8 @@ function getById(id) {
 // 
 // 
 // 
-export function get(_path, attributes, nested) {
+exports[`get`] = get
+function get(_path, attributes, nested) {
 
   if (attributes === true) {
     attributes = null
@@ -572,7 +709,7 @@ export function get(_path, attributes, nested) {
 // 
 function getExpandedCollection(_path, id, attributes, tag, nested) {
   let probableCollection = collections.get(id)
-
+  // console.log(_path, tag)
   if (probableCollection) {
 
 
@@ -598,7 +735,7 @@ function getExpandedCollection(_path, id, attributes, tag, nested) {
 // 
 // 
 // 
-export function getNested(path, attributes) {
+function getNested(path, attributes) {
   return get(path, attributes, true)
 }
 
@@ -637,30 +774,55 @@ function objectMethod(...args) {
 }
 
 
-export function subState(_path) {
-  const wrap = (method) => (path, ...args) => method.apply(null, [`${_path}${path}`, ...args])
-  return [
-    get, set, merge, on
-  ].reduce((acc, fn) => {
-    acc[fn.name] = wrap(fn)
-    return acc
-  }, {})
+
+const wrapWithPath = (method, _path, ctx = null) => 
+  (path, ...args) => {
+    const result = method.apply(null, [`${_path}${path}`, ...args])
+    return ctx || result
+  }
+
+const wrapWithoutPath = (method, _path) =>
+  (...args) => method.apply(null, [_path, ...args])
+
+
+function Context(path) {
+  this.path = path
+  ;
+  [set, merge, on, off, emit, notify, add, tag, untag, once].forEach(fn => {
+    this[fn.name] = wrapWithPath(fn, path, this).bind(this)
+  })
+  ;
+  [get].forEach(fn => {
+    this[fn.name] = wrapWithPath(fn, path).bind(this)
+  })
 }
 
 
-// export {
+exports[`context`] = context
+function context(path) {
+  return new Context(path)
+}
+
+
+// {
 //   notify as emit
 // }
 
-export const __internal__for__debug__purpose__only__ = isDev ? {
-  pathToIdsMap,
-  virtualTree,
-  objectEventsMap,
-  objectToIdsMap,
-  globalEventsMap,
-} : {}
 
-global.set = set
-global.get = get
-global.emit = notify
-global.on = on
+
+
+exports[`__internal__for__debug__purpose__only__`] = isDev ? {
+  'pathToIdsMap':pathToIdsMap,
+  'virtualTree':virtualTree,
+  'objectEventsMap':objectEventsMap,
+  'objectToIdsMap':objectToIdsMap,
+  'globalEventsMap':globalEventsMap,
+} : {}
+ 
+
+
+
+// global.set = set
+// global.get = get
+// global.emit = notify
+// global.on = on
